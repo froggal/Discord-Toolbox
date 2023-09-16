ade778f219bbd289815c68625c310f561ab1438c
keyfrog@outlook.kr <keyfrog@outlook.kr>
2023-09-16 11:42:54 +0900

    봇의 기본적인 코드 작성

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.all()
+app = commands.Bot(command_prefix='$', intents=intents)
+
+@app.event
+async def on_ready():
+    print('Connected to Discord')
+    await app.change_presence(status=discord.Status.online activity=discord.Game("$도움말"))
+
+@app.command()
+async def 안녕(ctx):
+    await ctx.send("안녕하세요")
+
+app.run(os.getenv("DICOTOKEN"))
