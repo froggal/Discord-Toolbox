225334221981e77b9c85f3c75316aeb0825afd85
keyfrog@outlook.kr <keyfrog@outlook.kr>
2023-09-16 11:44:10 +0900

    쉼표..

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = commands.Bot(command_prefix='$', intents=intents)
 @app.event
 async def on_ready():
     print('Connected to Discord')
-    await app.change_presence(status=discord.Status.online activity=discord.Game("$도움말"))
+    await app.change_presence(status=discord.Status.online, activity=discord.Game("$도움말"))
 
 @app.command()
 async def 안녕(ctx):
