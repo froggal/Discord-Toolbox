85762b8b73711d35fb5435f7b76214f318be1af2
keyfrog@outlook.kr <keyfrog@outlook.kr>
2023-09-16 12:10:40 +0900

    계산기 명령어 생성

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import discord
 from discord.ext import commands
-import random
+from baekjoon import boj
+from baekjoon import solvedac
+from baekjoon import problem
 
 intents = discord.Intents.all()
 app = commands.Bot(command_prefix='$', intents=intents)
@@ -18,6 +20,23 @@ async def on_ready():
 async def 도움말(ctx):
     await ctx.reply(embed=embed("도움말", "도움말은 여기서 확인해주세요!"))
 
-
+@app.command()
+async def 사칙연산(ctx, type, left: int, right: int):
+    if type == '덧셈':
+        data = left + right
+        await ctx.reply(
+            embed=embed('사칙연산 - 덧셈', f'{left}+{right} = {data}'))
+    elif type == '뺄셈':
+        data = left - right
+        await ctx.reply(
+            embed=embed('사칙연산 - 뺄셈', f'{left}-{right} = {data}'))
+    elif type == '곱셈':
+        data = left * right
+        await ctx.reply(
+            embed=embed('사칙연산 - 곱셈', f'{left}X{right} = {data}'))
+    elif type == '나눗셈':
+        data = left / right
+        await ctx.reply(
+            embed=embed('사칙연산 - 나눗셈', f'{left}/{right} = {data}'))
 
 app.run(os.getenv("DICOTOKEN"))
